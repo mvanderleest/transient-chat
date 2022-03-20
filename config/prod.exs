584c2f8354b94fa18a77cfc3eaa867dbb8d9ac78
mvanderleest <morganv@hey.com>
2022-03-19 21:32:23 -0500

    add specific url host to prod config

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ import Config
 # before starting your production server.
 config :transient_chat, TransientChatWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "transient-chat-prod.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
